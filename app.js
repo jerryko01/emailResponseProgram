97612ef8bc2225c3f62f88eff204bef2b145483d
jerryko01 <85685925+jerryko01@users.noreply.github.com>
2022-11-01 15:08:38 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+let hrefAttribute = "mailto:";
+let submitBtn = document.querySelector("#submit");
+let customBtn = document.querySelector("#custom");
+let name = document.querySelector(".name");
+let location = document.querySelector(".location");
+let email = document.querySelector(".email");
+let subject = document.querySelector(".subject")
 
+
+// Customization
+customBtn.addEventListener("click", function() {
+    
+})
+
+
+//Submission
+submitBtn.addEventListener("click", function() {
+  
+})
